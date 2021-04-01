91e647161bcde345fcfe647bb943c38ff2de1a5b
ahmmohfat18 <ahmmohfat18@gmail.com>
2021-04-02 01:56:11 +0200

    LAST COMMIT!!!

--- Picture-Viewer/Viewer.Designer.cs
+++ Picture-Viewer/Viewer.Designer.cs
@@ -37,19 +37,19 @@ namespace Picture_Viewer
             this.btnRemoveAllPictures = new System.Windows.Forms.Button();
             this.statusStrip = new System.Windows.Forms.StatusStrip();
             this.pictureName = new System.Windows.Forms.ToolStripStatusLabel();
+            this.space1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.operationalMode = new System.Windows.Forms.ToolStripStatusLabel();
             this.operationalModes = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.exit = new System.Windows.Forms.ToolStripMenuItem();
             this.singleMode = new System.Windows.Forms.ToolStripMenuItem();
             this.multiPictureMode = new System.Windows.Forms.ToolStripMenuItem();
-            this.defaultMode = new System.Windows.Forms.ToolStripMenuItem();
             this.slideshowMode = new System.Windows.Forms.ToolStripMenuItem();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnLeft = new System.Windows.Forms.Button();
             this.btnRight = new System.Windows.Forms.Button();
-            this.panel = new System.Windows.Forms.Panel();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.timer = new System.Windows.Forms.Timer(this.components);
+            this.space2 = new System.Windows.Forms.ToolStripStatusLabel();
             this.statusStrip.SuspendLayout();
             this.operationalModes.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -68,16 +68,18 @@ namespace Picture_Viewer
             // 
             // listBox
             // 
+            this.listBox.Enabled = false;
             this.listBox.FormattingEnabled = true;
             this.listBox.Location = new System.Drawing.Point(12, 43);
             this.listBox.Name = "listBox";
-            this.listBox.Size = new System.Drawing.Size(200, 329);
+            this.listBox.Size = new System.Drawing.Size(200, 381);
             this.listBox.TabIndex = 2;
             this.listBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ListBox_MouseClick);
             // 
             // btnRemovePicture
             // 
-            this.btnRemovePicture.Location = new System.Drawing.Point(12, 378);
+            this.btnRemovePicture.Enabled = false;
+            this.btnRemovePicture.Location = new System.Drawing.Point(12, 430);
             this.btnRemovePicture.Name = "btnRemovePicture";
             this.btnRemovePicture.Size = new System.Drawing.Size(200, 25);
             this.btnRemovePicture.TabIndex = 3;
@@ -87,7 +89,8 @@ namespace Picture_Viewer
             // 
             // btnRemoveAllPictures
             // 
-            this.btnRemoveAllPictures.Location = new System.Drawing.Point(12, 409);
+            this.btnRemoveAllPictures.Enabled = false;
+            this.btnRemoveAllPictures.Location = new System.Drawing.Point(12, 461);
             this.btnRemoveAllPictures.Name = "btnRemoveAllPictures";
             this.btnRemoveAllPictures.Size = new System.Drawing.Size(200, 25);
             this.btnRemoveAllPictures.TabIndex = 4;
@@ -98,23 +101,31 @@ namespace Picture_Viewer
             // statusStrip
             // 
             this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.operationalMode,
-            this.pictureName});
-            this.statusStrip.Location = new System.Drawing.Point(0, 439);
+            this.space1,
+            this.pictureName,
+            this.space2,
+            this.operationalMode});
+            this.statusStrip.Location = new System.Drawing.Point(0, 489);
             this.statusStrip.Name = "statusStrip";
-            this.statusStrip.Size = new System.Drawing.Size(734, 22);
+            this.statusStrip.Size = new System.Drawing.Size(784, 22);
             this.statusStrip.TabIndex = 5;
-            // 
-            // operationalMode
-            // 
-            this.operationalMode.Name = "operationalMode";
-            this.operationalMode.Text = "Default mode";
-            this.operationalMode.Size = new System.Drawing.Size(79, 17);
             // 
             // pictureName
             // 
             this.pictureName.Name = "pictureName";
             this.pictureName.Size = new System.Drawing.Size(0, 17);
+            // 
+            // space1
+            // 
+            this.space1.Name = "space1";
+            this.space1.Size = new System.Drawing.Size(457, 17);
+            this.space1.Text = "                                                  ";
+            // 
+            // operationalMode
+            // 
+            this.operationalMode.Name = "operationalMode";
+            this.operationalMode.Size = new System.Drawing.Size(73, 17);
+            this.operationalMode.Text = "Single mode";
             // 
             // operationalModes
             // 
@@ -122,97 +133,83 @@ namespace Picture_Viewer
             this.exit,
             this.singleMode,
             this.multiPictureMode,
-            this.defaultMode,
             this.slideshowMode});
-            this.operationalModes.Name = "contextMenuStrip";
-            this.operationalModes.Size = new System.Drawing.Size(179, 114);
+            this.operationalModes.Name = "operationalModes";
+            this.operationalModes.ShowImageMargin = false;
+            this.operationalModes.Size = new System.Drawing.Size(154, 92);
             this.operationalModes.Text = "Operational modes";
             // 
             // exit
             // 
             this.exit.Name = "exit";
-            this.exit.Size = new System.Drawing.Size(178, 22);
+            this.exit.Size = new System.Drawing.Size(153, 22);
             this.exit.Text = "Exit";
             this.exit.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // singleMode
             // 
+            this.singleMode.Enabled = false;
             this.singleMode.Name = "singleMode";
-            this.singleMode.Size = new System.Drawing.Size(178, 22);
+            this.singleMode.Size = new System.Drawing.Size(153, 22);
             this.singleMode.Text = "Single mode";
+            this.singleMode.Visible = false;
             this.singleMode.Click += new System.EventHandler(this.singleMode_Click);
             // 
             // multiPictureMode
             // 
+            this.multiPictureMode.Enabled = false;
             this.multiPictureMode.Name = "multiPictureMode";
-            this.multiPictureMode.Size = new System.Drawing.Size(178, 22);
+            this.multiPictureMode.Size = new System.Drawing.Size(153, 22);
             this.multiPictureMode.Text = "Multi-Picture mode";
             this.multiPictureMode.Click += new System.EventHandler(this.multiPictureMode_Click);
             // 
-            // defaultMode
-            // 
-            this.defaultMode.Name = "defaultMode";
-            this.defaultMode.Size = new System.Drawing.Size(178, 22);
-            this.defaultMode.Text = "Default Mode";
-            this.defaultMode.Visible = false;
-            this.defaultMode.Click += new System.EventHandler(this.defaultMode_Click);
-            // 
             // slideshowMode
             // 
+            this.slideshowMode.Enabled = false;
             this.slideshowMode.Name = "slideshowMode";
-            this.slideshowMode.Size = new System.Drawing.Size(178, 22);
+            this.slideshowMode.Size = new System.Drawing.Size(153, 22);
             this.slideshowMode.Text = "Slideshow mode";
             this.slideshowMode.Click += new System.EventHandler(this.slideshowMode_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Enabled = false;
             this.pictureBox1.Location = new System.Drawing.Point(218, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(504, 360);
+            this.pictureBox1.Size = new System.Drawing.Size(554, 412);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.Visible = false;
             // 
             // btnLeft
             // 
             this.btnLeft.BackgroundImage = global::Picture_Viewer.Properties.Resources.Left_icon;
             this.btnLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnLeft.Location = new System.Drawing.Point(218, 378);
+            this.btnLeft.Enabled = false;
+            this.btnLeft.Location = new System.Drawing.Point(218, 430);
             this.btnLeft.Name = "btnLeft";
             this.btnLeft.Size = new System.Drawing.Size(75, 56);
             this.btnLeft.TabIndex = 8;
-            this.btnLeft.Visible = false;
             this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
             // 
             // btnRight
             // 
             this.btnRight.BackgroundImage = global::Picture_Viewer.Properties.Resources.Right_icon;
             this.btnRight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnRight.Location = new System.Drawing.Point(647, 378);
+            this.btnRight.Enabled = false;
+            this.btnRight.Location = new System.Drawing.Point(697, 430);
             this.btnRight.Name = "btnRight";
             this.btnRight.Size = new System.Drawing.Size(75, 56);
             this.btnRight.TabIndex = 7;
-            this.btnRight.Visible = false;
             this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
-            // 
-            // panel
-            // 
-            this.panel.AutoScroll = true;
-            this.panel.BackColor = System.Drawing.Color.Transparent;
-            this.panel.Location = new System.Drawing.Point(218, 12);
-            this.panel.Name = "panel";
-            this.panel.Size = new System.Drawing.Size(504, 422);
-            this.panel.TabIndex = 9;
-            this.panel.Visible = false;
             // 
             // pictureBox2
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox2.Location = new System.Drawing.Point(12, 12);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(710, 422);
+            this.pictureBox2.Size = new System.Drawing.Size(760, 474);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 10;
             this.pictureBox2.TabStop = false;
@@ -223,13 +220,21 @@ namespace Picture_Viewer
             this.timer.Interval = 1000;
             this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
+            // space2
+            // 
+            this.space2.Name = "space2";
+            this.space2.Size = new System.Drawing.Size(0, 17);
+            this.space2.Text = "                                                                                 " +
+    "                                                                     ";
+            // 
             // Viewer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.BackgroundImage = global::Picture_Viewer.Properties.Resources.Background;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(734, 461);
+            this.ClientSize = new System.Drawing.Size(784, 511);
             this.ContextMenuStrip = this.operationalModes;
             this.Controls.Add(this.btnViewPictures);
             this.Controls.Add(this.listBox);
@@ -239,7 +244,6 @@ namespace Picture_Viewer
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnLeft);
             this.Controls.Add(this.btnRight);
-            this.Controls.Add(this.panel);
             this.Controls.Add(this.pictureBox2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -272,9 +276,9 @@ namespace Picture_Viewer
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button btnLeft;
         private System.Windows.Forms.Button btnRight;
-        private System.Windows.Forms.ToolStripMenuItem defaultMode;
-        private System.Windows.Forms.Panel panel;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.ToolStripStatusLabel space1;
+        private System.Windows.Forms.ToolStripStatusLabel space2;
     }
 }
--- Picture-Viewer/Viewer.cs
+++ Picture-Viewer/Viewer.cs
@@ -12,6 +12,7 @@ namespace Picture_Viewer
             this.InitializeComponent();
 
             this.pictures = new List<string>();
+            this.multiPicturePictures = new List<PictureBox>();
         }
         private void btnViewPictures_Click(object sender, EventArgs e)
         {
@@ -28,9 +29,25 @@ namespace Picture_Viewer
                 {
                     this.pictures.AddRange(picturesFiles.FileNames);
                     this.listBox.Items.AddRange(picturesFiles.SafeFileNames);
-                }
 
-                this.listBox.SelectedIndex = 0;
+                    this.listBox.Enabled = true;
+                    this.btnLeft.Enabled = true;
+                    this.btnRight.Enabled = true;
+                    this.singleMode.Enabled = true;
+                    this.slideshowMode.Enabled = true;
+                    this.multiPictureMode.Enabled = true;
+                    this.pictureBox1.Enabled = true;
+                    this.btnRemovePicture.Enabled = true;
+                    this.btnRemoveAllPictures.Enabled = true;
+
+                    if (!this.singleMode.Visible)
+                    {
+                        this.listBox.SelectedIndex = 0;
+                    }
+
+                    this.pictureBox1.ImageLocation = this.pictures[this.listBox.SelectedIndex];
+                    this.pictureName.Text = this.listBox.SelectedItem.ToString();
+                }
             }
             catch (Exception ex)
             {
@@ -39,60 +56,96 @@ namespace Picture_Viewer
         }
         private void ListBox_MouseClick(object sender, EventArgs e)
         {
-            if (this.pictureBox1.Visible == false && this.panel.Visible == false)
-            {
-                return;
-            }
-            if (this.panel.Visible == false)
+            if (this.pictureBox1.Visible)
             {
                 this.pictureBox1.ImageLocation = this.pictures[this.listBox.SelectedIndex];
                 this.pictureName.Text = this.listBox.SelectedItem.ToString();
+
                 return;
             }
+
             if (this.listBox.SelectedIndices.Count == 1)
             {
-                this.panel.Controls.Clear();
-
-                this.panel.Controls.Add(new PictureBox
+                for (int i = 0; i < this.multiPicturePictures.Count; i++)
                 {
-                    Name = "pictureBox0",
-                    Top = 6,
-                    Left = 6,
-                    Width = 234,
-                    Height = 234,
+                    this.Controls.Remove(multiPicturePictures[i]);
+                }
+                this.multiPicturePictures.Clear();
+
+                var picture = new PictureBox
+                {
+                    Name = "pictureBox",
+                    Location = new Point(218, 12),
+                    Width = 174,
+                    Height = 214,
                     Visible = true,
                     BackColor = Color.Transparent,
                     SizeMode = PictureBoxSizeMode.Zoom,
                     ImageLocation = this.pictures[this.listBox.SelectedIndex]
-                });
+                };
+
+                this.Controls.Add(picture);
+                this.multiPicturePictures.Add(picture);
 
                 return;
             }
 
-            this.panel.Controls.Clear();
+            for (int i = 0; i < this.multiPicturePictures.Count; i++)
+            {
+                this.Controls.Remove(multiPicturePictures[i]);
+            }
+            this.multiPicturePictures.Clear();
 
             for (int i = 0; i < this.listBox.SelectedIndices.Count; i++)
             {
-                this.panel.Controls.Add(new PictureBox
+                var picture = new PictureBox
                 {
-                    Name = "pictureBox" + i,
-                    Width = 234,
-                    Height = 234,
+                    Name = "pictureBox",
+                    Width = 174,
+                    Height = 214,
                     Visible = true,
-                    Top = 6 + (240 * (i / 2)),
-                    Left = 6 + (240 * (i % 2)),
                     BackColor = Color.Transparent,
                     SizeMode = PictureBoxSizeMode.Zoom,
-                    ImageLocation = this.pictures[this.listBox.SelectedIndices[i]]
-                });
+                    ImageLocation = this.pictures[this.listBox.SelectedIndices[i]],
+                    Location = new Point(218 + (180 * (i % 3)), 12 + (220 * (i / 3)))
+                };
+
+                this.Controls.Add(picture);
+                this.multiPicturePictures.Add(picture);
             }
         }
         private void btnRemovePicture_Click(object sender, EventArgs e)
         {
             try
             {
+                if (this.listBox.Items.Count == 1)
+                {
+                    this.pictures.Clear();
+                    this.listBox.Items.Clear();
+                    this.pictureName.Text = "";
+                    this.pictureBox1.ImageLocation = null;
+
+                    this.listBox.Enabled = false;
+                    this.btnLeft.Enabled = false;
+                    this.btnRight.Enabled = false;
+                    this.pictureBox1.Enabled = false;
+                    this.btnRemovePicture.Enabled = false;
+                    this.btnRemoveAllPictures.Enabled = false;
+
+                    this.singleMode.Enabled = false;
+                    this.slideshowMode.Enabled = false;
+                    this.multiPictureMode.Enabled = false;
+
+                    return;
+                }
+
                 var index = listBox.SelectedIndex;
+
                 this.listBox.SelectedIndex = this.listBox.SelectedIndex + 1 == this.listBox.Items.Count ? 0 : this.listBox.SelectedIndex + 1;
+                
+                this.pictureBox1.ImageLocation = this.pictures[this.listBox.SelectedIndex];
+                this.pictureName.Text = this.listBox.SelectedItem.ToString();
+
                 this.pictures.RemoveAt(index);
                 this.listBox.Items.RemoveAt(index);
             }
@@ -109,93 +162,118 @@ namespace Picture_Viewer
                 return;
             }
 
-            this.listBox.Items.Clear();
             this.pictures.Clear();
+            this.listBox.Items.Clear();
+            this.pictureName.Text = "";
+            this.pictureBox1.ImageLocation = null;
+
+            this.listBox.Enabled = false;
+            this.btnLeft.Enabled = false;
+            this.btnRight.Enabled = false;
+            this.pictureBox1.Enabled = false;
+            this.btnRemovePicture.Enabled = false;
+            this.btnRemoveAllPictures.Enabled = false;
+
+            this.singleMode.Enabled = false;
+            this.slideshowMode.Enabled = false;
+            this.multiPictureMode.Enabled = false;
         }
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (MessageBox.Show("Are You Sure you want to exit?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
-            {
-                Application.Exit();
-            }
+            this.Close();
         }
         private void singleMode_Click(object sender, EventArgs e)
         {
-            if (this.listBox.Items.Count < 1)
-            {
-                MessageBox.Show("Please select one or more picture to view", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-
-            this.defaultMode.Visible = true;
-
-            this.singleMode.Visible = false;
-            this.slideshowMode.Visible = false;
-            this.multiPictureMode.Visible = false;
-
             this.operationalMode.Text = "Single mode";
+            this.singleMode.Visible = false;
 
-            this.listBox.Height = 394;
+            this.slideshowMode.Visible = true;
+            this.multiPictureMode.Visible = true;
+
+            this.timer.Stop();
+            this.pictureBox2.Visible = false;
+
+            for (int i = 0; i < this.multiPicturePictures.Count; i++)
+            {
+                this.Controls.Remove(multiPicturePictures[i]);
+            }
+            this.multiPicturePictures.Clear();
+
+            this.btnLeft.Visible = true;
+            this.btnRight.Visible = true;
+            this.btnViewPictures.Visible = true;
+            this.btnRemovePicture.Visible = true;
+            this.btnRemoveAllPictures.Visible = true;
+
+            this.listBox.Height = 381;
+            this.listBox.Visible = true;
+            this.listBox.SelectedIndex = 0;
+            this.listBox.SelectionMode = SelectionMode.One;
 
             this.pictureBox1.Visible = true;
             this.pictureBox1.ImageLocation = this.pictures[this.listBox.SelectedIndex];
 
             this.pictureName.Visible = true;
             this.pictureName.Text = this.listBox.SelectedItem.ToString();
-
-            this.btnLeft.Visible = true;
-            this.btnRight.Visible = true;
-
-            this.btnRemovePicture.Visible = false;
-            this.btnRemoveAllPictures.Visible = false;
         }
         private void multiPictureMode_Click(object sender, EventArgs e)
         {
-            if (this.listBox.Items.Count < 1)
-            {
-                MessageBox.Show("Please select one or more picture to view", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-
-            this.defaultMode.Visible = true;
-
-            this.singleMode.Visible = false;
-            this.slideshowMode.Visible = false;
+            this.operationalMode.Text = "Multi-Pictures mode";
             this.multiPictureMode.Visible = false;
 
-            this.operationalMode.Text = "Multi-Pictures mode";
+            this.singleMode.Visible = true;
+            this.slideshowMode.Visible = true;
 
-            this.panel.Visible = true;
+            this.btnViewPictures.Visible = true;
 
-            this.listBox.Height = 394;
+            this.timer.Stop();
+            this.pictureBox2.Visible = false;
+
+            this.btnLeft.Visible = false;
+            this.btnRight.Visible = false;
+            this.pictureBox1.Visible = false;
+            this.pictureName.Visible = false;
+            this.btnRemovePicture.Visible = false;
+            this.btnRemoveAllPictures.Visible = false;
+
+            for (int i = 0; i < this.multiPicturePictures.Count; i++)
+            {
+                this.Controls.Remove(multiPicturePictures[i]);
+            }
+            this.multiPicturePictures.Clear();
+
+            this.listBox.Height = 446;
+            this.listBox.Visible = true;
+            this.listBox.SelectedIndex = 0;
             this.listBox.SelectionMode = SelectionMode.MultiExtended;
 
             this.ListBox_MouseClick(sender, e);
-
-            this.btnRemovePicture.Visible = false;
-            this.btnRemoveAllPictures.Visible = false;
         }
         private void slideshowMode_Click(object sender, EventArgs e)
         {
-            if (this.listBox.Items.Count < 1)
-            {
-                MessageBox.Show("Please select one or more picture to view", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-
-            this.defaultMode.Visible = true;
-
-            this.singleMode.Visible = false;
-            this.slideshowMode.Visible = false;
-            this.multiPictureMode.Visible = false;
-
             this.operationalMode.Text = "Slideshow mode";
+            this.slideshowMode.Visible = false;
+
+            this.singleMode.Visible = true;
+            this.multiPictureMode.Visible = true;
+
+            for (int i = 0; i < this.multiPicturePictures.Count; i++)
+            {
+                this.Controls.Remove(multiPicturePictures[i]);
+            }
+            this.multiPicturePictures.Clear();
 
             this.listBox.Visible = false;
-
+            this.btnLeft.Visible = false;
+            this.btnRight.Visible = false;
+            this.pictureBox1.Visible = false;
             this.btnViewPictures.Visible = false;
             this.btnRemovePicture.Visible = false;
             this.btnRemoveAllPictures.Visible = false;
+
+
+            this.listBox.SelectedIndex = 0;
+            this.listBox.SelectionMode = SelectionMode.One;
 
             this.pictureBox2.Visible = true;
             this.pictureBox2.ImageLocation = this.pictures[this.listBox.SelectedIndex];
@@ -219,43 +297,18 @@ namespace Picture_Viewer
             this.pictureBox1.ImageLocation = this.pictures[this.listBox.SelectedIndex];
             this.pictureName.Text = this.listBox.SelectedItem.ToString();
         }
-        private void defaultMode_Click(object sender, EventArgs e)
-        {
-            this.defaultMode.Visible = false;
-
-            this.singleMode.Visible = true;
-            this.slideshowMode.Visible = true;
-            this.multiPictureMode.Visible = true;
-
-            this.operationalMode.Text = "Default mode";
-
-            this.listBox.Height = 329;
-            this.listBox.Visible = true;
-            this.listBox.SelectedIndex = 0;
-            this.listBox.SelectionMode = SelectionMode.One;
-
-            this.timer.Stop();
-            this.panel.Controls.Clear();
-
-            this.panel.Visible = false;
-            this.btnLeft.Visible = false;
-            this.btnRight.Visible = false;
-            this.pictureBox1.Visible = false;
-            this.pictureBox2.Visible = false;
-            this.pictureName.Visible = false;
-
-            this.btnViewPictures.Visible = true;
-            this.btnRemovePicture.Visible = true;
-            this.btnRemoveAllPictures.Visible = true;
-        }
         private void timer_Tick(object sender, EventArgs e)
         {
-            this.listBox.SelectedIndex = this.listBox.SelectedIndex + 1 == this.listBox.Items.Count ? 0 : this.listBox.SelectedIndex + 1;
-            
-            this.pictureBox2.ImageLocation = this.pictures[this.listBox.SelectedIndex];
+            if (this.listBox.SelectedIndex + 1 == this.listBox.Items.Count)
+            {
+                this.timer.Stop();
+            }
+
+            this.pictureBox2.ImageLocation = this.pictures[++this.listBox.SelectedIndex];
             this.pictureName.Text = this.listBox.SelectedItem.ToString();
         }
 
         private List<string> pictures;
+        private List<PictureBox> multiPicturePictures;
     }
 }
